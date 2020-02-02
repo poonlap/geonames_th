291030b5544337003532962d77070a279fdab4aa
Poonlap V <poonlap@tanabutr.co.th>
2020-02-02 07:58:47 +0700

    Change info URL

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     """,
 
     'author': "Poonlap V.",
-    'website': "https://poonlap.github.io",
+    'website': "https://github.com/poonlap/geonames_th",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
