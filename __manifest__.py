894c97ebec35691229cac3aa61a5b907eb4ec929
Poonlap V <poonlap@tanabutr.co.th>
2020-02-02 09:34:57 +0700

    Versioning and added catagory

--- __manifest__.py
+++ __manifest__.py
@@ -19,8 +19,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Localization',
+    'version': '13.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base_location_geonames_import'],
